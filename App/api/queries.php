0ad1ea03c90d10282cfad731628d901f255664ff
jojo2234 <32978759+jojo2234@users.noreply.github.com>
2020-12-29 17:21:23 +0100

    Read description
    1.Usati array associativi per gestire le richieste jquery
    2.Risolto problema mappa che si sposta quando mi trovo con il mouse sopra i menu (esempio sopra lo slider).
    3.Messo in evidenza lo slider e cambiato nome.

--- App/api/queries.php
+++ App/api/queries.php
@@ -2,30 +2,35 @@
 include('../php/funzioni.php');
 header("Content-type: application/json");
 
-$funForQry = analizza_GET("funzione");
+//$funForQry = analizza_GET("funzione");
 
-/**Uno switch gestisce tutte le richieste GET ed effettua la query, la forma di una richiesta è funzione=NUM:[DATO] cioè 
- * un numero corrisponende alla query richiesta e un possibile campo opzionale di dati come anni valori o id.
- * Lo split viene effettuato in analizza_GET che si trova in funzioni.php così come la funzione esegui_query che effettua la connessione con il database*/
-switch($funForQry[0]){
+/**Uno switch gestisce tutte le richieste GET ed effettua la query, la forma di una richiesta è funzione=NUM&nomeParametro=[DATO]
+ *La funzione esegui_query che effettua la connessione con il database si trova in funzioni.php
+ */
+//echo json_encode(array("funzione"=>$_GET["funzione"],"mese"=>$_GET["mese"],"data"=>$_GET["data"]));
+//exit(1);
+
+switch($_GET["funzione"]){
 	case '1':
-		$sql = "SELECT DISTINCT mese FROM spi WHERE data='".$funForQry[1]."';";
+		$data = $_GET["data"];
+		$sql = "SELECT DISTINCT mese FROM spi WHERE data='".$data."';";
 		$campi = array('mese' => 'intval');
 	break;
 	case '2':
 	//Ritorna ID poligoni delle celle e valori SPI delle cellem per la data e il mese (in realtà il numero indica i mesi da prendere su cui viene fatta l'analisi) indicato
-	$sql = "SELECT celle.Id AS ID, Poligono, Centroide, SPI FROM celle JOIN spi ON ID_Cella = celle.Id WHERE Mese=".$funForQry[1]." AND Data='".$funForQry[2]."';";
+	$data = $_GET["data"];
+	$mese = $_GET["mese"];
+	$sql = "SELECT celle.Id AS ID, Poligono, Centroide, SPI FROM celle JOIN spi ON ID_Cella = celle.Id WHERE Mese=".$mese." AND Data='".$data."';";
 	$campi = array('ID' => 'intval','Poligono'=>'utf8_encode','Centroide'=>'utf8_encode','SPI'=>'floatval');
 	break;
 	case '3':
 	//Riporta le date disponibili per i mesi selezionati
 	$sql = "SELECT DISTINCT data FROM spi ORDER BY data DESC";
-	//$sql = "SELECT DISTINCT data FROM spi WHERE mese=".$funForQry[1];//".$funForQry['mse'];
 	$campi = array('data' => 'utf8_encode');
 	break;
-	default: 
+	default:
+	echo json_encode(array(	"status" => "error","dettagli" => "parametro mancante"));exit(1);
 	break;
 }
-
 echo esegui_query($sql, $campi);
 ?>
