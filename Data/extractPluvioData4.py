7000ba9ef4466ae0069ee466d429bfde1e235816
jojo2234 <32978759+jojo2234@users.noreply.github.com>
2021-01-22 16:26:03 +0100

    Preparing csv files to load data in DB
    Data ready to be loaded in db pluviosta143 and pluviosta71 not loaded because file size.

--- Data/extractPluvioData4.py
+++ Data/extractPluvioData4.py
@@ -11,13 +11,16 @@ a = 0 #Id precipitazione giornaliera
 csv_file = open("pluvosta71.csv", "w")
 stringa = ""
 for x in range(10,sht.nrows):
-    for y in range(1,71):
+    for y in range(3,72):
         a+=1
-        if(str(sht.cell(x,y).value) == "None" or str(sht.cell(x,y).value) == "-9999" or str(sht.cell(x,y).value) == "-9999.0"):
+        if(str(sht.cell(x,y).value) == "None" or str(sht.cell(x,y).value) == "-9999" or str(sht.cell(x,y).value) == "" or str(sht.cell(x,y).value) == "-9999.0"):
             vals = 0
         else:
             vals = sht.cell(x,y).value
             #Questo file ha anno mese e giorno separati in questordine
-        stringa += str(a)+";"+str(int(sht.cell(x,2).value))+"/"+str(int(sht.cell(x,1).value))+"/"+str(int(sht.cell(x,0).value))+";"+str(vals)+";"+str(sht.cell(1,y).value)+"\n"
+            idSta = sht.cell(1,y).value
+            if(idSta == None or idSta==""):
+                idSta = 0
+            stringa += str(a)+";"+str(int(sht.cell(x,2).value))+"/"+str(int(sht.cell(x,1).value))+"/"+str(int(sht.cell(x,0).value))+";"+str(vals)+";"+str(int(idSta))+"\n"
 csv_file.write(stringa)
 csv_file.close()
