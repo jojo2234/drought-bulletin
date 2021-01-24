42896686f3138749adde2a5ee4c778fc2c6a10fb
jojo2234 <32978759+jojo2234@users.noreply.github.com>
2021-01-24 12:01:51 +0100

    How to show pluviometric data?
    Work in progress...

--- App/api/queries.php
+++ App/api/queries.php
@@ -15,23 +15,27 @@ switch($_GET["funzione"]){
 		$data = $_GET["data"];
 		$sql = "SELECT DISTINCT mese FROM spi WHERE data='".$data."';";
 		$campi = array('mese' => 'intval');
-	break;
+		break;
 	case '2':
 	//Ritorna ID poligoni delle celle e valori SPI delle cellem per la data e il mese (in realtà il numero indica i mesi da prendere su cui viene fatta l'analisi) indicato
-	$data = $_GET["data"];
-	$mese = $_GET["mese"];
-	$sql = "SELECT celle.Id AS ID, Poligono, Centroide, SPI FROM celle JOIN spi ON ID_Cella = celle.Id WHERE Mese=".$mese." AND Data='".$data."';";
-	$campi = array('ID' => 'intval','Poligono'=>'utf8_encode','Centroide'=>'utf8_encode','SPI'=>'floatval');
-	break;
+		$data = $_GET["data"];
+		$mese = $_GET["mese"];
+		$sql = "SELECT celle.Id AS ID, Poligono, Centroide, SPI FROM celle JOIN spi ON ID_Cella = celle.Id WHERE Mese=".$mese." AND Data='".$data."';";
+		$campi = array('ID' => 'intval','Poligono'=>'utf8_encode','Centroide'=>'utf8_encode','SPI'=>'floatval');
+		break;
 	case '3':
-	//Riporta le date disponibili per i mesi selezionati
-	$sql = "SELECT DISTINCT data FROM spi ORDER BY data DESC";
-	$campi = array('data' => 'utf8_encode');
-	break;
+		//Riporta le date disponibili per i mesi selezionati
+		$sql = "SELECT DISTINCT data FROM spi ORDER BY data DESC;";
+		$campi = array('data' => 'utf8_encode');
+		break;
 	case '4':
-	$sql = "SELECT Latitudine, Longitudine, Nome, ID FROM stazione";
-	$campi = array('Latitudine' => 'floatval', 'Longitudine' => 'floatval', 'Nome' => 'utf8_encode', 'ID' => 'intval');
-	break;
+		$sql = "SELECT Latitudine, Longitudine, Nome, ID FROM stazione;";
+		$campi = array('Latitudine' => 'floatval', 'Longitudine' => 'floatval', 'Nome' => 'utf8_encode', 'ID' => 'intval');
+		break;
+	case '5':
+		$sql = "SELECT Nome, QuotaDTM, ID_Distretto FROM stazione WHERE ID=".$_GET["id"].";";
+		$campi = array('Nome' => 'utf8_encode', 'QuotaDTM' => 'intval', 'ID_Distretto' => 'intval');
+		break;
 	default:
 	echo json_encode(array(	"status" => "error","dettagli" => "parametro mancante"));exit(1);
 	break;
